8e14bb8c2fc68b406dca1259dfd2d299754df2aa
Gio <gohan1703@gmail.com>
2024-01-31 19:40:49 -0500

    FPS Cam

--- Assets/Scripts/FPSTerrainGeneration/FirstPersonCam.cs
+++ Assets/Scripts/FPSTerrainGeneration/FirstPersonCam.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    //public PlayerController playerMovement;
-
-    public float senX;
-    public float senY;
-    public float multiplier = 1f;
-
-    //private Transform orientation;
-
-    private float xRot;
-    private float yRot;
-    private float zRot = 0f;
-    public float targetZRot = 0f;
-    public float lerpSpeed = 1f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -25,17 +11,4 @@ public class FirstPersonCamera : MonoBehaviour
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseX = (Input.GetAxis("Mouse X")) * Time.deltaTime * senX * multiplier;
-        float mouseY = (Input.GetAxis("Mouse Y")) * Time.deltaTime * senY * multiplier;
-
-        yRot += mouseX;
-        xRot -= mouseY;
-        xRot = Mathf.Clamp(xRot, -90f, 90f);
-        zRot = Mathf.Lerp(zRot, targetZRot, Time.deltaTime * lerpSpeed);
-        transform.rotation = Quaternion.Euler(xRot, yRot, zRot);
-        //orientation.rotation = Quaternion.Euler(0, yRot, 0);
-    }
 }
--- Assets/Scripts/FPSTerrainGeneration/SpecCam.cs
+++ Assets/Scripts/FPSTerrainGeneration/SpecCam.cs
@@ -13,20 +13,26 @@ public class SpecCam : MonoBehaviour
 
         // Calculate the movement direction based on the camera's forward and right vectors
         Vector3 moveDirection = (transform.forward * verticalMovement + transform.right * horizontalMovement).normalized;
+        //transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
 
         // Move the camera in the calculated direction
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+        //transform.position.x += (moveDirection.x * moveSpeed * Time.deltaTime);
+        //transform.position.z += (moveDirection.z * moveSpeed * Time.deltaTime);
+        transform.position = transform.position + new Vector3(moveDirection.x * moveSpeed * Time.deltaTime, 0, moveDirection.z * moveSpeed * Time.deltaTime);
+
+        //transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
 
         // Move the camera up when holding 'E'
         if (Input.GetKey(KeyCode.E))
         {
-            transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime, Space.World);
+            //transform.position.y += (Vector3.up * verticalSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime, Space.World);
         }
 
         // Move the camera down when holding 'Q'
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime, Space.World);
+            //transform.position.y += (Vector3.down * verticalSpeed * Time.deltaTime);
         }
     }
 }
