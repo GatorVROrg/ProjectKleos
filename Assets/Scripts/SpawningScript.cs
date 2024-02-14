d3e3aac0a62b7844c7040908dc438c6ceda32dc5
alam-anwar <93105876+alam-anwar@users.noreply.github.com>
2024-02-14 18:25:41 -0500

    spawning cubes
    starting point for resource; now need to work on collecting objects when player walks over them

---
+++ Assets/Scripts/SpawningScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningScript : MonoBehaviour
+{
+    public GameObject obj;
+    int randNum;
+    public Transform spawnDest1, spawnDest2, spawnDest3, spawnDest4;
+    public bool spawningBool = true;
+    public float spawnTime;
+
+    void Start() {
+        StartCoroutine(spawning());
+    }
+
+    IEnumerator spawning() {
+        while (spawningBool == true) {
+            yield return new WaitForSeconds(spawnTime);
+            randNum = Random.Range(0, 4);
+            if (randNum == 0) {
+                Instantiate(obj, spawnDest1.position, spawnDest1.rotation);
+            }
+            if (randNum == 1) {
+                Instantiate(obj, spawnDest2.position, spawnDest2.rotation);
+            }
+            if (randNum == 2) {
+                Instantiate(obj, spawnDest3.position, spawnDest3.rotation);
+            }
+            if (randNum == 3) {
+                Instantiate(obj, spawnDest4.position, spawnDest4.rotation);
+            }
+        }
+    }
+
+}
