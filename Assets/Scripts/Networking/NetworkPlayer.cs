c28dbc37b6c1b8daa287983845159426a19c36f2
Mileshaase <Mileshaase.imperium@gmail.com>
2024-02-16 18:43:52 -0500

    PENIS

--- Assets/Scripts/Networking/NetworkPlayer.cs
+++ Assets/Scripts/Networking/NetworkPlayer.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.Rendering;
-using Unity.VisualScripting;
-using System.Linq;
 
 public class NetworkPlayer : MonoBehaviour
 {
