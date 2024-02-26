636982726d728c63735d51fa36f65bdb11b7713b
abirle <abirle@ufl.edu>
2024-02-26 18:52:13 -0500

    this should work - al

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public bool inSlot;
+    public Vector3 slotRotation = Vector3.zero;
+    public InventorySlot currentSlot;
+}
