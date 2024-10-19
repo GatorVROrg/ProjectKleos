339119270a251a07e7c4a743dc46d67fcbbd21ad
Gio <44237862+Redifexx@users.noreply.github.com>
2024-10-18 21:17:18 -0400

    continuous movement/turn

--- Assets/ContinuousMovementPhysics.cs
+++ Assets/ContinuousMovementPhysics.cs
@@ -7,17 +7,24 @@ public class ContinuousMovementPhysics : MonoBehaviour
 {
 
     public float speed = 1.0f;
+    public float turnSpeed = 60f;
     public InputActionProperty moveInputSource;
+    public InputActionProperty turnInputSource;
     public Rigidbody rb;
     public LayerMask groundLayer;
     public Transform directionSource;
+    public Transform turnSource;
+
+
     public CapsuleCollider bodyCollider;
     private Vector2 inputMoveAxis;
+    private float inputTurnAxis;
 
     // Update is called once per frame
     void Update()
     {
         inputMoveAxis = moveInputSource.action.ReadValue<Vector2>();
+        inputTurnAxis = turnInputSource.action.ReadValue<Vector2>().x;
     }
 
     private void FixedUpdate()
@@ -29,8 +36,19 @@ public class ContinuousMovementPhysics : MonoBehaviour
             Quaternion yaw = Quaternion.Euler(0, directionSource.eulerAngles.y, 0);
             Vector3 direction = yaw * new Vector3(inputMoveAxis.x, 0, inputMoveAxis.y);
 
-            rb.MovePosition(rb.position + direction * Time.fixedDeltaTime * speed);
-        }
+            Vector3 targetMovePosition = rb.position + direction * Time.fixedDeltaTime * speed;
+
+            Vector3 axis = Vector3.up;
+            float angle = turnSpeed * Time.fixedDeltaTime * inputTurnAxis;
+
+            Quaternion q = Quaternion.AngleAxis(angle, axis);
+
+            rb.MoveRotation(rb.rotation * q);
+
+            Vector3 newPosition = q * (targetMovePosition - turnSource.position) + turnSource.position;
+
+            rb.MovePosition(newPosition);
+        } 
 
     }
 
